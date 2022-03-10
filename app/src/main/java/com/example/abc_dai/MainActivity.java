b2b3ebd6005b8145cc6ce04888d890363d7cd860
gui <guilhermebarbosa2002@live.com.pt>
2022-03-10 18:18:59 +0000

    alterei umas cenas

--- app/src/main/java/com/example/abc_dai/MainActivity.java
+++ app/src/main/java/com/example/abc_dai/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.abc_dai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonOnclick(View v){
+        TextView text = (TextView) findViewById(R.id.LOGIN_text);
+        EditText username= (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText password= (EditText) findViewById(R.id.editTextTextPassword);
+
+
+        if(username.getText().toString().isEmpty() ){
+            text.setText("Insira o nome!!");
+            username.requestFocus();
+
+        }else if(password.getText().toString().isEmpty() ){
+            text.setText("Insira a password!!");
+            password.requestFocus();
+
+        }else{
+
+            text.setText("LOGIN EFETUADO");
+        }
+
+    }
+
+    public void fecharApp(View v){
+        TextView text = (TextView) findViewById(R.id.LOGIN_text);
+        text.setText("DESLIGADO");
     }
 }
